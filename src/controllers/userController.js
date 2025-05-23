5e636a38674769fce35a72e8d287ac292418cdee
MateusC2 <mateuscubeiro.educ@gmail.com>
2025-05-22 21:38:17 -0300

    correção de bug updateUser

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,9 +32,10 @@ module.exports = class userController {
           console.error(err);
           return res.status(500).json({ error: "Erro ao cadastrar usuário" });
         }
-        return res.status(201).json({ message: "Usuário cadastrado com sucesso" });
+        return res
+          .status(201)
+          .json({ message: "Usuário cadastrado com sucesso" });
       });
-
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: "Erro interno do servidor" });
@@ -77,15 +78,16 @@ module.exports = class userController {
         }
 
         // Se o CPF e a senha estiverem corretos, login bem-sucedido
-        const token = jwt.sign({id: user.id_usuario}, process.env.SECRET, {expiresIn:"1h"});
+        const token = jwt.sign({ id: user.id_usuario }, process.env.SECRET, {
+          expiresIn: "1h",
+        });
 
         //Remove um atributo de um objeto
         delete user.password;
 
-        return res.status(200).json({message:"Login bem sucedido",
-          user,
-          token
-        })
+        return res
+          .status(200)
+          .json({ message: "Login bem sucedido", user, token });
       });
     } catch (error) {
       console.error(error);
@@ -93,137 +95,142 @@ module.exports = class userController {
     }
   }
 
-
-//Próximas funções
-static async getAllUsers(req, res) {
-  const query = `SELECT * FROM usuario`;
-  try {
-    connect.query(query, function (err, results) {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Erro interno do Servidor" });
-      }
-      return res.status(200).json({ message: "Lista de Usuários", users: results });
-    });
-  } catch (error) {
-    console.error("Erro ao executar consulta:", error);
-    return res.status(500).json({ error: "Erro interno do Servidor" });
-  }
-}
-
-
-static async updateUser(req, res) {
-  const { id_usuario, nome, telefone, email, senha, cpf } = req.body;
-  const userId = id_usuario;
-  const VerificarToken = userId;
-
-  // Aqui você precisa ter certeza que `req.user.id` existe, ou adaptar conforme seu token
-  if (VerificarToken !== req.userId) {
-    return res.status(403).json({ error: "Usuário não autorizado a atualizar este perfil" });
-  }
-
-  // Verificar se todos os campos necessários foram preenchidos
-  if (!id_usuario || !nome || !telefone || !email || !senha || !cpf) {
-    return res.status(400).json({ error: "Todos os campos devem ser preenchidos" });
-  }
-
-  try {
-    // Validar se CPF já está cadastrado para outro usuário
-    const cpfError = await validateCpf(cpf, id_usuario);
-    if (cpfError) return res.status(400).json(cpfError);
-
-    // Validar se Email já está cadastrado para outro usuário
-    const emailError = await validateEmail(email, id_usuario);
-    if (emailError) return res.status(400).json(emailError);
-
-    // Atualizar os dados no banco
-    const queryUpdate = `UPDATE usuario SET nome=?, telefone=?, email=?, senha=?, cpf=? WHERE id_usuario = ?`;
-    const valuesUpdate = [nome, telefone, email, senha, cpf, id_usuario];
-
-    connect.query(queryUpdate, valuesUpdate, (err, results) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Erro ao atualizar usuário" });
-      }
-      return res.status(200).json({ message: "Usuário atualizado com sucesso" });
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Erro interno do servidor" });
-  }
-}
-
-
-
-static async deleteUser(req, res) {
-  const userId = req.params.id; // ID que veio da URL
-  const usuarioId = req.userId.id; // ID do usuário autenticado (via token)
-
-  // Verifica se o usuário autenticado está tentando deletar outro usuário
-  if (Number(userId) !== Number(usuarioId)) {
-    return res.status(403).json({ error: "Usuário não autorizado a deletar este perfil" });
-  }
-  
-  const queryDelete = `DELETE FROM usuario WHERE id_usuario=?`;
-  const values = [userId];
-
-  try {
-    connect.query(queryDelete, values, function (err, results) {
-      if (err) {
-        if (err.code === 'ER_ROW_IS_REFERENCED_2') {
-          // Tratamento do erro de chave estrangeira
-          console.error('Erro de integridade referencial:', err);
-          return res.status(400).json({ error: "Impossível deletar conta. Reservas relacionadas." });
+  //Próximas funções
+  static async getAllUsers(req, res) {
+    const query = `SELECT * FROM usuario`;
+    try {
+      connect.query(query, function (err, results) {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Erro interno do Servidor" });
         }
-        console.error('Erro ao deletar usuário:', err);
-        return res.status(500).json({ error: "Erro interno no servidor" });
-      }
-      if (results.affectedRows === 0) {
-        return res.status(404).json({ error: "Usuário não encontrado" });
-      }
-      return res.status(200).json({ message: "Usuário excluído com sucesso" });
-    });
-  } catch (error) {
-    console.error('Erro inesperado:', error);
-    return res.status(500).json({ error: "Erro interno do servidor" });
-  }
-}
-
-
-static async getUserById(req, res) {
-  const userId = req.params.id_usuario; // ID que veio da URL
-
-  if (!userId) {
-    return res.status(400).json({ error: "ID do usuário é obrigatório" });
+        return res
+          .status(200)
+          .json({ message: "Lista de Usuários", users: results });
+      });
+    } catch (error) {
+      console.error("Erro ao executar consulta:", error);
+      return res.status(500).json({ error: "Erro interno do Servidor" });
+    }
   }
 
-  const query = `SELECT * FROM usuario WHERE id_usuario = ?`;
+  static async updateUser(req, res) {
+    const { id_usuario, nome, telefone, email, senha, cpf } = req.body;
+    const userAuthId = req.userId.id; // ID do usuário autenticado pelo token
 
-  connect.query(query, [userId], function (err, results) {
-    if (err) {
-      console.error("Erro ao buscar usuário:", err);
+    if (id_usuario !== userAuthId) {
+      return res
+        .status(403)
+        .json({ error: "Usuário não autorizado a atualizar este perfil" });
+    }
+    // Verificar se todos os campos necessários foram preenchidos
+    if (!id_usuario || !nome || !telefone || !email || !senha || !cpf) {
+      return res
+        .status(400)
+        .json({ error: "Todos os campos devem ser preenchidos" });
+    }
+
+    try {
+      // Validar se CPF já está cadastrado para outro usuário
+      const cpfError = await validateCpf(cpf, id_usuario);
+      if (cpfError) return res.status(400).json(cpfError);
+
+      // Validar se Email já está cadastrado para outro usuário
+      const emailError = await validateEmail(email, id_usuario);
+      if (emailError) return res.status(400).json(emailError);
+
+      // Atualizar os dados no banco
+      const queryUpdate = `UPDATE usuario SET nome=?, telefone=?, email=?, senha=?, cpf=? WHERE id_usuario = ?`;
+      const valuesUpdate = [nome, telefone, email, senha, cpf, id_usuario];
+
+      connect.query(queryUpdate, valuesUpdate, (err, results) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: "Erro ao atualizar usuário" });
+        }
+        return res
+          .status(200)
+          .json({ message: "Usuário atualizado com sucesso" });
+      });
+    } catch (error) {
+      console.error(error);
       return res.status(500).json({ error: "Erro interno do servidor" });
     }
+  }
 
-    if (results.length === 0) {
-      return res.status(404).json({ error: "Usuário não encontrado" });
+  static async deleteUser(req, res) {
+    const userId = req.params.id; // ID que veio da URL
+    const userAuthId = req.userId.id; // ID do usuário autenticado pelo token
+
+    // Verifica se o usuário autenticado está tentando deletar outro usuário
+    if (userId !== userAuthId) {
+      return res
+        .status(403)
+        .json({ error: "Usuário não autorizado a deletar este perfil" });
     }
 
-    // Pega o primeiro usuário encontrado
-    const user = results[0];
+    const queryDelete = `DELETE FROM usuario WHERE id_usuario=?`;
+    const values = [userId];
 
-    return res.status(200).json({
-      user: {
-        id: user.id_usuario,
-        nome: user.nome,
-        email: user.email,
-        telefone: user.telefone,
-        cpf: user.cpf,
-      },
+    try {
+      connect.query(queryDelete, values, function (err, results) {
+        if (err) {
+          if (err.code === "ER_ROW_IS_REFERENCED_2") {
+            // Tratamento do erro de chave estrangeira
+            console.error("Erro de integridade referencial:", err);
+            return res
+              .status(400)
+              .json({
+                error: "Impossível deletar conta. Reservas relacionadas.",
+              });
+          }
+          console.error("Erro ao deletar usuário:", err);
+          return res.status(500).json({ error: "Erro interno no servidor" });
+        }
+        if (results.affectedRows === 0) {
+          return res.status(404).json({ error: "Usuário não encontrado" });
+        }
+        return res
+          .status(200)
+          .json({ message: "Usuário excluído com sucesso" });
+      });
+    } catch (error) {
+      console.error("Erro inesperado:", error);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  }
+
+  static async getUserById(req, res) {
+    const userId = req.params.id_usuario; // ID que veio da URL
+
+    if (!userId) {
+      return res.status(400).json({ error: "ID do usuário é obrigatório" });
+    }
+
+    const query = `SELECT * FROM usuario WHERE id_usuario = ?`;
+
+    connect.query(query, [userId], function (err, results) {
+      if (err) {
+        console.error("Erro ao buscar usuário:", err);
+        return res.status(500).json({ error: "Erro interno do servidor" });
+      }
+
+      if (results.length === 0) {
+        return res.status(404).json({ error: "Usuário não encontrado" });
+      }
+
+      // Pega o primeiro usuário encontrado
+      const user = results[0];
+
+      return res.status(200).json({
+        user: {
+          id: user.id_usuario,
+          nome: user.nome,
+          email: user.email,
+          telefone: user.telefone,
+          cpf: user.cpf,
+        },
+      });
     });
-  });
-}
-
+  }
 };
-
-
